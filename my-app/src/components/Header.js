c976f568e9868125f5a7101f357524abf92c566b
Klemense <klemenseloh@gmail.com>
2022-03-11 10:02:58 -0500

    2.0

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -5,7 +5,8 @@ function Header() {
     return (
     <header> 
         <img className="header_img" src="https://cdn.shopify.com/s/files/1/0246/5542/9666/files/charity_header.png?v=1565249421" alt="Charity" />
-        
+        <h2 className="title"> Charity Finder </h2>
+        <h3 className="subtitle"> An easier way to create impact!</h3>
     </header>
     )
 }
