32e5a75f47e08238b01be1cfd33cd24032e6b1dd
Klemense <klemenseloh@gmail.com>
2022-03-11 10:56:01 -0500

    3.0

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <header> 
         <img className="header_img" src="https://cdn.shopify.com/s/files/1/0246/5542/9666/files/charity_header.png?v=1565249421" alt="Charity" />
         <h2 className="title"> Charity Finder </h2>
-        <h3 className="subtitle"> An easier way to create impact!</h3>
+        <h3 className="subtitle"> CharityFinder allows you to search for reputable charities by your preferred category, giving you a brief description about them and also letting you save them to your personal list. Website also shows ratings for each charity, as determined by Charity Navigator.</h3>
     </header>
     )
 }
